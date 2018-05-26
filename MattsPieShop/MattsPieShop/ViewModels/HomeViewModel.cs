c5ef0d0464f6aa09936777510fd00b2d6377edd6
MattHall2 <MattHall2@users.noreply.github.com>
2018-05-26 19:50:52 -0400

    Creating the list page

---
+++ MattsPieShop/MattsPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MattsPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MattsPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
